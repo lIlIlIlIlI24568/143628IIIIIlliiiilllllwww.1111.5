587aa47c53841f4d5132ebc09ee41f744e2f14d2
xgomn <3641643802@qq.com>
2024-10-19 19:23:54 +0800

    Update xgoui 汉化修复.lua

--- xgoui 汉化修复.lua
+++ xgoui 汉化修复.lua
@@ -1,3 +1,69 @@
+local lp = game.Players.LocalPlayer
+
+local animationInfo = {}
+
+function getInfo(id)
+  local success, info = pcall(function()
+      return game:GetService("MarketplaceService"):GetProductInfo(id)
+  end)
+  if success then
+      return info
+  end
+  return {Name=''}
+end
+function block(player)
+  keypress(0x46)
+  wait()
+  keyrelease(0x46)
+end
+
+local AnimNames = {
+  'Slash',
+  'Swing',
+  'Sword'
+}
+
+function playerAdded(v)
+    local function charadded(char)
+      local humanoid = char:WaitForChild("Humanoid", 5)
+      if humanoid then
+          humanoid.AnimationPlayed:Connect(function(track)
+              local info = animationInfo[track.Animation.AnimationId]
+              if not info then
+                  info = getInfo(tonumber(track.Animation.AnimationId:match("%d+")))
+                  animationInfo[track.Animation.AnimationId] = info
+              end
+             
+              if (lp.Character and lp.Character:FindFirstChild("Head") and v.Character:FindFirstChild("Head")) then
+                  local mag = (v.Character.Head.Position - lp.Character.Head.Position).Magnitude
+                  if mag < 15  then
+                     
+                      for _, animName in pairs(AnimNames) do
+                          if info.Name:match(animName) then
+                              pcall(block, v)
+                          end
+                      end
+                     
+                  end
+              end
+          end)
+      end
+  end
+ 
+  if v.Character then
+      charadded(v.Character)
+  end
+  v.CharacterAdded:Connect(charadded)
+end
+
+for i,v in pairs(game.Players:GetPlayers()) do
+   if v ~= lp then
+       playerAdded(v)
+   end
+end
+
+game.Players.PlayerAdded:Connect(playerAdded)
+end
 -- DomainX the most rushed and money making focused script available
 -- now open source because we aren't money hungry
 
@@ -4952,50 +5018,3 @@ while true do
 	FpsLabel.Text = tostring(UpdatedFPS)
 	Domain.Main.Time.Text = tostring(GetDate():format("#h:#m"))
 end
-local UserInputService = game:GetService("UserInputService")
-
-local function IsShiftKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.RightShift)
-end
-
-local function IsCtrlKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.RightControl)
-end
-
-local function IsAltKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.RightAlt)
-end
-
-local function IsJKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.J)
-end
-
-local function IsKKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.K)
-end
-
-local function IsDelKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.Delete)
-end
-
-local function IsPgdKeyDown()
-	return UserInputService:IsKeyDown(Enum.KeyCode.PageDown)
-end
-
-local function Input(input, gameProcessedEvent)
-	if IsShiftKeyDown() then
-		if Combat.Visible == true then
-			Combat.Visible = false
-			Farming.Visible = false
-			Misc.Visible = false
-			Visual.Visible = false
-		else
-			Combat.Visible = true
-			Farming.Visible = true
-			Misc.Visible = true
-			Visual.Visible = true
-		end
-	end
-end
-
-UserInputService.InputBegan:Connect(Input)
